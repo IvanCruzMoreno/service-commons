faa16d07aa7f9cf78849fbc8541edf66559baa0f
ivan <ivanmoreno405@gmail.com>
2021-01-12 11:15:49 -0600

    this is to have all commons entities in a single project

---
+++ service-commons/src/main/java/com/badbadcode/application/commons/ServiceCommonsApplication.java
@@ -0,0 +1,11 @@
+package com.badbadcode.application.commons;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCommonsApplication {
+
+	
+
+}
